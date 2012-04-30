521ec27d38b1503385330ce1576818ff7ffec658
Solomon Foster <colomon@gmail.com>
2012-04-29 22:39:56 -0400

    Change export(:DEFAULT) to just plain export.

--- lib/Math/Vector.pm
+++ lib/Math/Vector.pm
@@ -36,12 +36,12 @@ class Math::Vector
         @.coordinates.elems;
     }
 
-    multi sub infix:<⋅>(Math::Vector $a, Math::Vector $b where { $a.Dim == $b.Dim }) is export(:DEFAULT) # is tighter(&infix:<+>) (NYI)
+    multi sub infix:<⋅>(Math::Vector $a, Math::Vector $b where { $a.Dim == $b.Dim }) is export # is tighter(&infix:<+>) (NYI)
     {
         [+]($a.coordinates »*« $b.coordinates);
     }
 
-    multi sub infix:<dot>(Math::Vector $a, Math::Vector $b) is export(:DEFAULT)
+    multi sub infix:<dot>(Math::Vector $a, Math::Vector $b) is export
     {
         $a ⋅ $b;
     }
@@ -69,44 +69,44 @@ class Math::Vector
         }
     }
     
-    multi sub infix:<+> (Math::Vector $a, Math::Vector $b where { $a.Dim == $b.Dim }) is export(:DEFAULT)
+    multi sub infix:<+> (Math::Vector $a, Math::Vector $b where { $a.Dim == $b.Dim }) is export
     {
         Math::Vector.new($a.coordinates »+« $b.coordinates);
     }
     
-    multi sub infix:<->(Math::Vector $a, Math::Vector $b where { $a.Dim == $b.Dim }) is export(:DEFAULT)
+    multi sub infix:<->(Math::Vector $a, Math::Vector $b where { $a.Dim == $b.Dim }) is export
     {
         Math::Vector.new($a.coordinates »-« $b.coordinates);
     }
 
-    multi sub prefix:<->(Math::Vector $a) is export(:DEFAULT)
+    multi sub prefix:<->(Math::Vector $a) is export
     {
         Math::Vector.new(0 <<-<< $a.coordinates);
     }
 
-    multi sub infix:<*>(Math::Vector $a, $b) is export(:DEFAULT)
+    multi sub infix:<*>(Math::Vector $a, $b) is export
     {
         Math::Vector.new($a.coordinates >>*>> $b);
     }
 
-    multi sub infix:<*>($a, Math::Vector $b) is export(:DEFAULT)
+    multi sub infix:<*>($a, Math::Vector $b) is export
     {
         Math::Vector.new($a <<*<< $b.coordinates);
     }
 
-    multi sub infix:</>(Math::Vector $a, $b) is export(:DEFAULT)
+    multi sub infix:</>(Math::Vector $a, $b) is export
     {
         Math::Vector.new($a.coordinates >>/>> $b);
     }
 
-    multi sub infix:<×>(Math::Vector $a where { $a.Dim == 3 }, Math::Vector $b where { $b.Dim == 3 }) is export(:DEFAULT)
+    multi sub infix:<×>(Math::Vector $a where { $a.Dim == 3 }, Math::Vector $b where { $b.Dim == 3 }) is export
     {
         Math::Vector.new($a.coordinates[1] * $b.coordinates[2] - $a.coordinates[2] * $b.coordinates[1], 
                    $a.coordinates[2] * $b.coordinates[0] - $a.coordinates[0] * $b.coordinates[2], 
                    $a.coordinates[0] * $b.coordinates[1] - $a.coordinates[1] * $b.coordinates[0]);
     }
 
-    multi sub infix:<×>(Math::Vector $a where { $a.Dim == 7 }, Math::Vector $b where { $b.Dim == 7 }) is export(:DEFAULT)
+    multi sub infix:<×>(Math::Vector $a where { $a.Dim == 7 }, Math::Vector $b where { $b.Dim == 7 }) is export
     {
         Math::Vector.new($a.coordinates[1] * $b.coordinates[3] - $a.coordinates[3] * $b.coordinates[1] 
                    + $a.coordinates[2] * $b.coordinates[6] - $a.coordinates[6] * $b.coordinates[2] 
@@ -131,17 +131,17 @@ class Math::Vector
                    + $a.coordinates[3] * $b.coordinates[4] - $a.coordinates[4] * $b.coordinates[3]);
     }
 
-    multi sub infix:<cross>(Math::Vector $a, Math::Vector $b) is export(:DEFAULT)
+    multi sub infix:<cross>(Math::Vector $a, Math::Vector $b) is export
     {
         $a × $b;
     }
 
-    multi sub circumfix:<⎡ ⎤>(Math::Vector $a) is export(:DEFAULT)
+    multi sub circumfix:<⎡ ⎤>(Math::Vector $a) is export
     {
         $a.Length;
     }
 
-    sub is_approx_vector(Math::Vector $a, Math::Vector $b, $desc) is export(:DEFAULT)
+    sub is_approx_vector(Math::Vector $a, Math::Vector $b, $desc) is export
     {
         ok(($a - $b).Length < 0.00001, $desc);
     }
